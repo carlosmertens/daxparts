24163a5b844a76cd83b79acdb3fbacba941f963b
Carlos Mertens <mertenscarlos@yahoo.com>
2020-07-09 00:00:55 +0200

    Convert App to a class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 // Bootstrap imports
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,15 +7,15 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 // Component imports
 import Navbar from './components/navbar/Navbar';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <div>
-        <h1 className='display-2'>Sanity Check!!!</h1>
-      </div>
-    </>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <Router>
+        <Route path='/' component={Navbar} />
+      </Router>
+    );
+  }
 }
 
 export default App;
