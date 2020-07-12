8de7baa4ba0cd1d7d5c879ccd1762a1492ca2d0a
Carlos Mertens <mertenscarlos@yahoo.com>
2020-07-12 12:44:58 +0200

    Setup Tutorial title

--- src/components/tutorial/Tutorial.js
+++ src/components/tutorial/Tutorial.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Tutorial.css';
 
 function Tutorial() {
   return (
-    <div>
-      <h1>Tutoriales</h1>
+    <div className='container-fluid tutorial-contenido'>
+      <div className='container tutorial-title'>
+        <h1>Tutoriales</h1>
+      </div>
     </div>
   );
 }
