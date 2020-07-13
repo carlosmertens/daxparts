7b1a5f220d776d83a9a3c216c6d554fc38fe32fe
Carlos Mertens <mertenscarlos@yahoo.com>
2020-07-13 23:49:42 +0200

    Add table to Cotizacion

--- src/components/cotizacion/Cotizacion.js
+++ src/components/cotizacion/Cotizacion.js
@@ -9,7 +9,7 @@ class Cotizacion extends Component {
     this.state = { buscar: [] };
   }
 
-  // Llamar back-end para obtener busqueda de parte
+  // Llamar back-end (server) para obtener busqueda de parte
   async componentDidMount() {
     const buscarData = await axios.get(
       'https://jsonplaceholder.typicode.com/users/2'
@@ -29,6 +29,45 @@ class Cotizacion extends Component {
       <div className='container-fluid cotizacion-contenido'>
         <div className='container cotizacion-title'>
           <h1>Resultado</h1>
+        </div>
+
+        <div className='container cotizacion-table'>
+          <div className='table-responsive'>
+            <table className='table table-striped table-bordered'>
+              <thead className='thead-dark'>
+                <tr>
+                  <th scope='col'>Descripcion</th>
+                  <th scope='col'>Aplicacion</th>
+                  <th scope='col'>Tipo</th>
+                  <th scope='col'>Precio</th>
+                  <th scope='col'>Tiempo de Entrega</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope='row'>descripcion test</th>
+                  <td>aplicacion test</td>
+                  <td>tipo test</td>
+                  <td>precio test</td>
+                  <td>entrega test</td>
+                </tr>
+                <tr>
+                  <th scope='row'>Test 2</th>
+                  <td>Test 2</td>
+                  <td>Test 2</td>
+                  <td>Test 2</td>
+                  <td>Test 2</td>
+                </tr>
+                <tr>
+                  <th scope='row'>descripcion 3</th>
+                  <td>aplicacion 3</td>
+                  <td>tipo 3</td>
+                  <td>precio 3</td>
+                  <td>entrega 3</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     );
