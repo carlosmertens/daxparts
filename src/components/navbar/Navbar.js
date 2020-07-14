2b3e80442c4185e5c3931819c45e0d935e4b3406
Carlos Mertens <mertenscarlos@yahoo.com>
2020-07-14 11:51:29 +0200

    Create modal for Ingresar button

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import navLogo from '../../images/logoNavWhite.png';
+import navLogo from '../../images/logoNav.png';
+import navLogoWhite from '../../images/logoNavWhite.png';
 
 class Navbar extends Component {
   render() {
     return (
       <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
         <Link to='/' className='navbar-brand'>
-          <img src={navLogo} alt='Dax Logo' />
+          <img src={navLogoWhite} alt='Dax Logo' />
         </Link>
         <button
           className='navbar-toggler'
@@ -44,20 +45,52 @@ class Navbar extends Component {
               </Link>
             </li>
           </ul>
-          <form className='form-inline my-2 my-lg-0'>
-            {/* <input
-              className='mr-sm-2'
-              type='text'
-              placeholder='Usuario'
-              aria-label='Search'
-            /> */}
+
+          {/* Boton de ingresar */}
+          <div className='form-inline my-2 my-lg-0'>
             <h4 className='text-white'>Ya tienes una cuenta?</h4>
+            {/* Button trigger modal */}
             <button
-              className='btn btn-outline-success my-2 my-sm-0'
-              type='submit'>
+              type='button'
+              class='btn'
+              data-toggle='modal'
+              data-target='#ingresarModal'>
               Ingresar
             </button>
-          </form>
+
+            {/* Modal */}
+            <div
+              className='modal fade'
+              id='ingresarModal'
+              tabindex='-1'
+              role='dialog'
+              aria-labelledby='exampleModalLabel'
+              aria-hidden='true'>
+              <div className='modal-dialog'>
+                <div className='modal-content align-items-center d-flex'>
+                  <div className='modal-header'>
+                    <img src={navLogo} alt='Dax Logo' />
+                    <button
+                      type='button'
+                      className='close'
+                      data-dismiss='modal'
+                      aria-label='Close'>
+                      <span aria-hidden='true'>&times;</span>
+                    </button>
+                  </div>
+                  <div className='modal-body'>...</div>
+                  <div className='modal-footer'>
+                    <button
+                      type='button'
+                      className='btn btn-secondary'
+                      data-dismiss='modal'>
+                      Ingresar
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </nav>
     );
