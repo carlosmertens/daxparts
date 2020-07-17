cf1abc9222d50bd52d66b5543f3c141ac88bc8f5
Carlos Mertens <mertenscarlos@yahoo.com>
2020-07-17 21:46:53 +0200

    refactor text with idioma props Contacto

--- src/components/contacto/Contacto.js
+++ src/components/contacto/Contacto.js
@@ -1,69 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Contacto.css';
 import usa from '../../images/usa.png';
 import bolivia from '../../images/bolivia.png';
 import peru from '../../images/peru.png';
 import paraguay from '../../images/paraguay.png';
 
-function Contacto() {
-  return (
-    <div className='container-fluid contacto-contenido'>
-      <div className='container contacto-title'>
-        <h1>Contáctanos!</h1>
-      </div>
+class Contacto extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    // Desestructuración del props idioma
+    const texto = { ...this.props.idioma };
+    return (
+      <div className='container-fluid contacto-contenido'>
+        <div className='container contacto-title'>
+          <h1>{texto.contacto.titulo}</h1>
+        </div>
 
-      <div className='container contacto-contenido'>
-        <div className='row'>
-          {/* Columna 1 (Banderas) */}
-          <div className='col-sm'>
-            <div className='row'>
-              {/* row banderas */}
-              <div className='col-md'>
-                <img src={bolivia} alt='Bandera Bolivia' />
-                <h4>Dax Parts Bolivia</h4>
-                <a href='mailto:fabiola@daxparts.com'>fabiola@daxparts.com</a>
-                <br />
-                <a href='tel:+59167898045'>+591-678-98045</a>
+        <div className='container contacto-contenido'>
+          <div className='row'>
+            {/* Columna 1 (Banderas) */}
+            <div className='col-sm'>
+              <div className='row'>
+                {/* row banderas */}
+                <div className='col-md'>
+                  <img src={usa} alt='Bandera Usa' />
+                  <h4>{texto.contacto.usa.nombre}</h4>
+                  <a href='mailto:dax@daxparts.com'>{texto.contacto.usa.email}</a>
+                  <br />
+                  <a href='tel:+59167898045'>{texto.contacto.usa.telefono}</a>
+                </div>
+                <div className='col-md'>
+                  <img src={bolivia} alt='Bandera Bolivia' />
+                  <h4>{texto.contacto.bolivia.nombre}</h4>
+                  <a href='mailto:fabiola@daxparts.com'>
+                    {texto.contacto.bolivia.email}
+                  </a>
+                  <br />
+                  <a href='tel:+59167898045'>{texto.contacto.bolivia.telefono}</a>
+                </div>
               </div>
-              <div className='col-md'>
-                <img src={peru} alt='Bandera Peru' />
-                <h4>Dax Parts Peru</h4>
-                <a href='mailto:em@daxparts.com'>em@daxparts.com</a>
-                <br />
-                <a href='tel:+59167898045'>+591-678-98045</a>
+
+              <div className='row'>
+                <div className='col-md'>
+                  <img src={peru} alt='Bandera Peru' />
+                  <h4>{texto.contacto.peru.nombre}</h4>
+                  <a href='mailto:em@daxparts.com'>{texto.contacto.peru.email}</a>
+                  <br />
+                  <a href='tel:+59167898045'>{texto.contacto.peru.telefono}</a>
+                </div>
+                <div className='col-md'>
+                  <img src={paraguay} alt='Bandera Paraguay' />
+                  <h4>{texto.contacto.paraguay.nombre}</h4>
+                  <a href='mailto:ventaspy@daxparts.com'>
+                    {texto.contacto.paraguay.email}
+                  </a>
+                  <br />
+                  <a href='tel:+59167898045'>
+                    {texto.contacto.paraguay.telefono}
+                  </a>
+                </div>
               </div>
             </div>
 
-            <div className='row'>
-              <div className='col-md'>
-                <img src={paraguay} alt='Bandera Paraguay' />
-                <h4>Dax Parts Paraguay</h4>
-                <a href='mailto:ventaspy@daxparts.com'>ventaspy@daxparts.com</a>
-                <br />
-                <a href='tel:+59167898045'>+591-678-98045</a>
-              </div>
-              <div className='col-md'>
-                <img src={usa} alt='Bandera Usa' />
-                <h4>Dax Parts Usa</h4>
-                <a href='mailto:dax@daxparts.com'>dax@daxparts.com</a>
-                <br />
-                <a href='tel:+59167898045'>+591-678-98045</a>
-              </div>
+            {/* Columna (Formulario) */}
+            <div className='col-sm'>
+              <p>{texto.contacto.contenido}</p>
             </div>
-          </div>
-
-          {/* Columna (Formulario) */}
-          <div className='col-sm'>
-            <p>
-              Nos gusta conocer y conversar con nuestros nuevos clientes. Comienza
-              enviándonos un mensaje y nos pondremos en contacto en Breve.
-            </p>
-            <h4>Formulario de contacto</h4>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Contacto;
