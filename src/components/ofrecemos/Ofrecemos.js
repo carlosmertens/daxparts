a240da9751d334a53a97a9ee5981aa9450cbc102
Carlos Mertens <mertenscarlos@yahoo.com>
2020-08-02 00:06:45 +0200

    Style Ofrecemos

--- src/components/ofrecemos/Ofrecemos.js
+++ src/components/ofrecemos/Ofrecemos.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import './Ofrecemos.css';
 
-// import excabadora from '../../images/excavadoraLogo.png';
-// import gruaLogo from '../../images/gruaLogo.png';
-// import agricolaLogo from '../../images/agricolaLogo.png';
-// import elecLogo from '../../images/elecLogo.png';
-// import camionLogo from '../../images/camionLogo.png';
-
 import { ReactComponent as Bulldozer } from '../../images/bulldozer.svg';
 import { ReactComponent as Crane } from '../../images/crane.svg';
 import { ReactComponent as Machine } from '../../images/machine.svg';
@@ -34,78 +28,90 @@ class Ofrecemos extends Component {
 
         {/* Grid de contenido */}
         <div className='container ofrecemos-contenido'>
-          <div className='row'>
+          <div className='row ofrecemos-opciones bg-danger align-items-center'>
             <div className='col-sm'>
               <div className='container opcion'>
-                <div className='float-left'>
-                  <Bulldozer />
-                </div>
-                <div className='flow-right'>
-                  <p>
-                    <span>{texto.ofrecemos.span.a1}</span> {texto.ofrecemos.span.a2}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className='col-sm'>
-              <div className='container'>
-                <div className='float-left'>
-                  <Crane />
-                </div>
-                <div className='flow-right'>
-                  <p>
-                    <span>{texto.ofrecemos.span.b1}</span> {texto.ofrecemos.span.b2}
-                  </p>
+                <div className='row'>
+                  <div className='col ofrecemos-icono bg-primary'>
+                    <Bulldozer />
+                  </div>
+                  <div className='col ofrecemos-texto bg-secondary'>
+                    <p>
+                      <span>{texto.ofrecemos.span.a1}</span> {texto.ofrecemos.span.a2}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div className='col-sm'>
-              <div className='container'>
-                <div className='float-left'>
-                  <Page />
+              <div className='container option'>
+                <div className='row'>
+                  <div className='col ofrecemos-icono bg-success'>
+                    <Crane />
+                  </div>
+                  <div className='col ofrecemos-texto bg-warning'>
+                    <p>
+                      <span>{texto.ofrecemos.span.b1}</span> {texto.ofrecemos.span.b2}
+                    </p>
+                  </div>
                 </div>
-                <div className='flow-right'>
-                  <p>
-                    <span>{texto.ofrecemos.span.c1}</span> {texto.ofrecemos.span.c2}
-                  </p>
+              </div>
+            </div>
+            <div className='col-sm'>
+              <div className='container option'>
+                <div className='row'>
+                  <div className='col ofrecemos-icono bg-primary'>
+                    <Page />
+                  </div>
+                  <div className='col ofrecemos-texto bg-secondary'>
+                    <p>
+                      <span>{texto.ofrecemos.span.c1}</span> {texto.ofrecemos.span.c2}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className='row'>
+          <div className='row ofrecemos-opciones bg-info align-items-center'>
             <div className='col-sm'>
-              <div className='container'>
-                <div className='float-left'>
-                  <Machine />
+              <div className='container option'>
+                <div className='row'>
+                  <div className='col ofrecemos-icono bg-secondary'>
+                    <Machine />
+                  </div>
+                  <div className='col ofrecemos-texto bg-primary'>
+                    <p>
+                      <span>{texto.ofrecemos.span.d1}</span> {texto.ofrecemos.span.d2}
+                    </p>
+                  </div>
                 </div>
-                <div className='flow-right'>
-                  <p>
-                    <span>{texto.ofrecemos.span.d1}</span> {texto.ofrecemos.span.d2}
-                  </p>
+              </div>
+            </div>
+            <div className='col-sm'>
+              <div className='container option'>
+                <div className='row'>
+                  <div className='col ofrecemos-icono bg-success'>
+                    <Tow />
+                  </div>
+                  <div className='col ofrecemos-texto bg-warning'>
+                    <p>
+                      <span>{texto.ofrecemos.span.e1}</span> {texto.ofrecemos.span.e2}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
             <div className='col-sm'>
               <div className='container'>
-                <div className='float-left'>
-                  <Tow />
-                </div>
-                <div className='flow-right'>
-                  <p>
-                    <span>{texto.ofrecemos.span.e1}</span> {texto.ofrecemos.span.e2}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className='col-sm'>
-              <div className='container'>
-                <div className='float-left'>
-                  <Truck />
-                </div>
-                <div className='flow-right'>
-                  <p>
-                    <span>{texto.ofrecemos.span.f1}</span> {texto.ofrecemos.span.f2}
-                  </p>
+                <div className='row'>
+                  <div className='col ofrecemos-icono bg-secondary'>
+                    <Truck />
+                  </div>
+                  <div className='col ofrecemos-texto bg-primary'>
+                    <p>
+                      <span>{texto.ofrecemos.span.f1}</span> {texto.ofrecemos.span.f2}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
