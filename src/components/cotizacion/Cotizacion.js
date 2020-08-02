c0b29098c02fdb1d12d815b5fe2b6cf31ab83853
Carlos Mertens <mertenscarlos@yahoo.com>
2020-08-02 21:21:18 +0200

    Refactor from Class to Hook

--- src/components/cotizacion/Cotizacion.js
+++ src/components/cotizacion/Cotizacion.js
@@ -1,107 +1,95 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Spinner from '../spinner/Spinner';
 import axios from 'axios';
 import config from '../../config';
 import './Cotizacion.css';
 import { Link } from 'react-router-dom';
 
-class Cotizacion extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { busqueda: [] };
-  }
-
-  componentDidMount() {
+const Cotizacion = (props) => {
+  const [busqueda, setBusqueda] = useState([]);
+  useEffect(() => {
     // Prueba con API de peliculas (TheMovieDB)
     const nowPlayingUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${config.api_key}`;
     axios.get(nowPlayingUrl).then((response) => {
       // console.log(response.data);
       const movieData = response.data.results;
-      this.setState({
-        busqueda: movieData,
-      });
+      setBusqueda(movieData);
     });
+  }, []);
+
+  // Mientras "buscar state" este vacio, spinner va a correr
+  if (busqueda.length === 0) {
+    return <Spinner />;
   }
-
-  render() {
-    // Mientras "buscar state" este vacio, spinner va a correr
-    if (this.state.busqueda.length === 0) {
-      return <Spinner />;
-    }
-    // Desestructuración del props idioma
-    const texto = { ...this.props.idioma };
-    // Bucle el resultado y formar las lineas de la tabla
-    const movieGrid = this.state.busqueda.map((item, index) => {
-      return (
-        <tr key={index}>
-          <th scope='row'>{item.title}</th>
-          <td>{item.id}</td>
-          <td>{item.original_language}</td>
-          <td>{item.vote_count}</td>
-          <td>{item.release_date}</td>
-        </tr>
-      );
-    });
-    console.log(this.props.parte);
+  // Desestructuración del props idioma
+  const texto = { ...props.idioma };
+  // Bucle el resultado y formar las lineas de la tabla
+  const movieGrid = busqueda.map((item, index) => {
     return (
-      <div className='container-fluid cotizacion-contenido'>
-        <div className='container cotizacion-title'>
-          <p>{texto.cotizacion.titulo}</p>
-        </div>
+      <tr key={index}>
+        <th scope='row'>{item.title}</th>
+        <td>{item.id}</td>
+        <td>{item.original_language}</td>
+        <td>{item.vote_count}</td>
+        <td>{item.release_date}</td>
+      </tr>
+    );
+  });
+  console.log(props.parte);
+  return (
+    <div className='container-fluid cotizacion-contenido'>
+      <div className='container cotizacion-title'>
+        <p>{texto.cotizacion.titulo}</p>
+      </div>
 
-        <div className='container cotizacion-table'>
-          <div className='table-responsive'>
-            <table className='table table-striped table-bordered'>
-              <thead className='thead-dark'>
-                <tr>
-                  <th scope='col'>{texto.cotizacion.tabla.col1}</th>
-                  <th scope='col'>{texto.cotizacion.tabla.col2}</th>
-                  <th scope='col'>{texto.cotizacion.tabla.col3}</th>
-                  <th scope='col'>{texto.cotizacion.tabla.col4}</th>
-                  <th scope='col'>{texto.cotizacion.tabla.col5}</th>
-                </tr>
-              </thead>
-              <tbody>{movieGrid}</tbody>
-            </table>
-            <div className='Container bg-secondary'>
-              <p className='nota text-white'>
-                Nombres OEM, logos y numeros de partes son usados para referencia.
-                No implicamos que nuestros productos o servicios estén afiliados,
-                patrocinados o aprobados por los fabricantes de equipos originales
+      <div className='container cotizacion-table'>
+        <div className='table-responsive'>
+          <table className='table table-striped table-bordered'>
+            <thead className='thead-dark'>
+              <tr>
+                <th scope='col'>{texto.cotizacion.tabla.col1}</th>
+                <th scope='col'>{texto.cotizacion.tabla.col2}</th>
+                <th scope='col'>{texto.cotizacion.tabla.col3}</th>
+                <th scope='col'>{texto.cotizacion.tabla.col4}</th>
+                <th scope='col'>{texto.cotizacion.tabla.col5}</th>
+              </tr>
+            </thead>
+            <tbody>{movieGrid}</tbody>
+          </table>
+          <div className='Container bg-secondary'>
+            <p className='nota text-white'>
+              Nombres OEM, logos y numeros de partes son usados para referencia. No
+              implicamos que nuestros productos o servicios estén afiliados,
+              patrocinados o aprobados por los fabricantes de equipos originales
+            </p>
+          </div>
+          <div className='container d-flex justify-content-center'>
+            <div className='beneficios bg-dark'>
+              <h4 className='titulo-jumbo'>Si eres usuario podras:</h4>
+              <p className='beneficios-jumbo'>Crear varias cotizaciones</p>
+              <p className='beneficios-jumbo'>
+                Ingresar cientos de codigos de manera sencilla
               </p>
-            </div>
-            <div className='container d-flex justify-content-center'>
-              <div className='beneficios bg-dark'>
-                <h4 className='titulo-jumbo'>Si eres usuario podras:</h4>
-                <p className='beneficios-jumbo'>Crear varias cotizaciones</p>
-                <p className='beneficios-jumbo'>
-                  Ingresar cientos de codigos de manera sencilla
-                </p>
-                <p className='beneficios-jumbo'>
-                  Email o imprimiar tus cotizaciones
-                </p>
-                <p className='beneficios-jumbo'>
-                  Hacer seguimientos a tus ordenes
-                </p>
-                <p className='beneficios-jumbo'>
-                  Acceder a miles de piezas usadas y reconstruidas
-                </p>
-                <p className='beneficios-jumbo'>
-                  Comprar repuestos de reemplazos de diferentes marcas
-                </p>
-                <p className='beneficios-jumbo'>y mucho mas...</p>
-                <Link to='/crear'>
-                  <button className='btn mb-2' type='button'>
-                    Crear
-                  </button>
-                </Link>
-              </div>
+              <p className='beneficios-jumbo'>Email o imprimiar tus cotizaciones</p>
+              <p className='beneficios-jumbo'>Hacer seguimientos a tus ordenes</p>
+              <p className='beneficios-jumbo'>
+                Acceder a miles de piezas usadas y reconstruidas
+              </p>
+              <p className='beneficios-jumbo'>
+                Comprar repuestos de reemplazos de diferentes marcas
+              </p>
+              <p className='beneficios-jumbo'>y mucho mas...</p>
+              <Link to='/crear'>
+                <button className='btn mb-2' type='button'>
+                  Crear
+                </button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Cotizacion;
--- src/components/somos/Somos.js
+++ src/components/somos/Somos.js
@@ -1,48 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Somos.css';
 
-class Somos extends Component {
-  constructor(props) {
-    super(props);
-    this.state = '';
-  }
+const Somos = ({ idioma }) => (
+  <div className='somos-contenido'>
+    <div className='container somos-title'>
+      <h1>{idioma.somos.titulo}</h1>
+    </div>
 
-  render() {
-    // Desestructuración del props idioma
-    const texto = { ...this.props.idioma };
-    return (
-      <div className='somos-contenido'>
-        <div className='container somos-title'>
-          <h1>{texto.somos.titulo}</h1>
-        </div>
+    <div className='container somos-texto'>
+      <p>{idioma.somos.contenido}</p>
+    </div>
 
-        <div className='container somos-texto'>
-          <p>{texto.somos.contenido}</p>
-        </div>
+    {/* <div className='container somos-title'></div> */}
 
-        {/* <div className='container somos-title'></div> */}
+    <div className='container somos-texto'>
+      <h4>{idioma.somos.titulo2}</h4>
+      <p>{idioma.somos.contenido2}</p>
+    </div>
 
-        <div className='container somos-texto'>
-          <h4>{texto.somos.titulo2}</h4>
-          <p>{texto.somos.contenido2}</p>
-        </div>
-
-        {/* <div className='container-fluid somos-banner'>
-          <div className='row'>
-            <div className='col'>
-              <h3>{texto.somos.banner.texto1}</h3>
-            </div>
-            <div className='col'>
-              <h3>{texto.somos.banner.texto2}</h3>
-            </div>
-            <div className='col'>
-              <h3>{texto.somos.banner.texto3}</h3>
-            </div>
+    {/* <div className='container-fluid somos-banner'>
+        <div className='row'>
+          <div className='col'>
+            <h3>{texto.somos.banner.texto1}</h3>
           </div>
-        </div> */}
-      </div>
-    );
-  }
-}
+          <div className='col'>
+            <h3>{texto.somos.banner.texto2}</h3>
+          </div>
+          <div className='col'>
+            <h3>{texto.somos.banner.texto3}</h3>
+          </div>
+        </div>
+      </div> */}
+  </div>
+);
 
 export default Somos;
