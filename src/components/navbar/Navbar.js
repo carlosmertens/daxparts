23f178ea61506cc4d2e2d60856a61be729c3165a
Carlos Mertens <mertenscarlos@yahoo.com>
2020-08-02 21:49:16 +0200

    Refactor Navbar from Class to Hook component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import navLogoWhite from '../../images/logoNavWhite.png';
@@ -9,84 +9,74 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import openModal from '../../actions/openModal';
 
-class Navbar extends Component {
-  constructor() {
-    super();
-    this.state = '';
-  }
-  render() {
-    // Desestructuración del props idioma
-    const texto = { ...this.props.idioma };
-    return (
-      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-        {/* Logo Navigador */}
-        <div className='navbar-brand'>
-          <Link to='/'>
-            <img src={navLogoWhite} alt='Dax Logo' />
+const Navbar = (props) => (
+  <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+    {/* Logo Navigador */}
+    <div className='navbar-brand'>
+      <Link to='/'>
+        <img src={navLogoWhite} alt='Dax Logo' />
+      </Link>
+    </div>
+
+    {/* Boton pantalla pequeña */}
+    <button
+      className='navbar-toggler'
+      type='button'
+      data-toggle='collapse'
+      data-target='#navbarSupportedContent'
+      aria-controls='navbarSupportedContent'
+      aria-expanded='false'
+      aria-label='Toggle navigation'>
+      <span className='navbar-toggler-icon'></span>
+    </button>
+
+    <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+      {/* Enlaces Introductorios */}
+      <ul className='navbar-nav mx-auto'>
+        <li className='nav-item'>
+          <Link to='/somos' className='nav-link text-white'>
+            <h4>{props.idioma.navbar.enlace1}</h4>
           </Link>
-        </div>
+        </li>
+        <li className='nav-item'>
+          <Link to='/hacemos' className='nav-link text-white' href='/'>
+            <h4>{props.idioma.navbar.enlace2}</h4>
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link to='ofrecemos' className='nav-link text-white' href='/'>
+            <h4>{props.idioma.navbar.enlace3}</h4>
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link to='contacto' className='nav-link text-white' href='/'>
+            <h4>{props.idioma.navbar.enlace4}</h4>
+          </Link>
+        </li>
+      </ul>
 
-        {/* Boton pantalla pequeña */}
+      {/* Botones modal de Ingresar y Crear Cuenta */}
+      <div className='form-inline my-2 my-lg-0'>
         <button
-          className='navbar-toggler'
           type='button'
-          data-toggle='collapse'
-          data-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'>
-          <span className='navbar-toggler-icon'></span>
+          className='btn'
+          onClick={() => {
+            props.openModal('open', <Ingresar />);
+          }}>
+          {props.idioma.navbar.botonTexto}
         </button>
-
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          {/* Enlaces Introductorios */}
-          <ul className='navbar-nav mx-auto'>
-            <li className='nav-item'>
-              <Link to='/somos' className='nav-link text-white'>
-                <h4>{texto.navbar.enlace1}</h4>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/hacemos' className='nav-link text-white' href='/'>
-                <h4>{texto.navbar.enlace2}</h4>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='ofrecemos' className='nav-link text-white' href='/'>
-                <h4>{texto.navbar.enlace3}</h4>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='contacto' className='nav-link text-white' href='/'>
-                <h4>{texto.navbar.enlace4}</h4>
-              </Link>
-            </li>
-          </ul>
-
-          {/* Botones modal de Ingresar y Crear Cuenta */}
-          <div className='form-inline my-2 my-lg-0'>
-            <button
-              type='button'
-              className='btn'
-              onClick={() => {
-                this.props.openModal('open', <Ingresar />);
-              }}>
-              {texto.navbar.botonTexto}
-            </button>
-            <button
-              type='button'
-              className='btn'
-              onClick={() => {
-                this.props.openModal('open', <Crear />);
-              }}>
-              Crear cuenta
-            </button>
-          </div>
-        </div>
-      </nav>
-    );
-  }
-}
+        <button
+          type='button'
+          className='btn'
+          onClick={() => {
+            props.openModal('open', <Crear />);
+          }}>
+          Crear cuenta
+        </button>
+      </div>
+    </div>
+  </nav>
+);
 
 // Action dispacher para llamar modal
 function mapDispatchToProps(dispacher) {
