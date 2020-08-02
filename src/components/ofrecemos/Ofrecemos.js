4b118570d9da44f574f35b0bfded30fa20a86652
Carlos Mertens <mertenscarlos@yahoo.com>
2020-08-02 21:33:27 +0200

    Refactor Ofrecemos from Class to Hook component

--- src/components/ofrecemos/Ofrecemos.js
+++ src/components/ofrecemos/Ofrecemos.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Ofrecemos.css';
 
 import { ReactComponent as Bulldozer } from '../../images/bulldozer.svg';
@@ -8,118 +8,108 @@ import { ReactComponent as Page } from '../../images/Page-1.svg';
 import { ReactComponent as Tow } from '../../images/tow-truck.svg';
 import { ReactComponent as Truck } from '../../images/truck.svg';
 
-class Ofrecemos extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    // Desestructuración del props idioma
-    const texto = { ...this.props.idioma };
-    return (
-      <div className='container-fluid ofrecemos-contenido'>
-        <div className='container ofrecemos-title'>
-          <h1>{texto.ofrecemos.titulo}</h1>
-        </div>
+const Ofrecemos = ({ idioma }) => (
+  <div className='container-fluid ofrecemos-contenido'>
+    <div className='container ofrecemos-title'>
+      <h1>{idioma.ofrecemos.titulo}</h1>
+    </div>
 
-        <div className='container ofrecemos-subtitle'>
-          <h4>{texto.ofrecemos.subtitulo}</h4>
-        </div>
+    <div className='container ofrecemos-subtitle'>
+      <h4>{idioma.ofrecemos.subtitulo}</h4>
+    </div>
 
-        {/* Grid de contenido */}
-        <div className='container ofrecemos-contenido'>
-          <div className='row ofrecemos-opciones bg-danger align-items-center'>
-            <div className='col-sm'>
-              <div className='container opcion'>
-                <div className='row'>
-                  <div className='col ofrecemos-icono bg-primary'>
-                    <Bulldozer />
-                  </div>
-                  <div className='col ofrecemos-texto bg-secondary'>
-                    <p>
-                      <span>{texto.ofrecemos.span.a1}</span> {texto.ofrecemos.span.a2}
-                    </p>
-                  </div>
-                </div>
+    {/* Grid de contenido */}
+    <div className='container ofrecemos-contenido'>
+      <div className='row ofrecemos-opciones align-items-center'>
+        <div className='col-sm'>
+          <div className='container opcion'>
+            <div className='row'>
+              <div className='col ofrecemos-icono'>
+                <Bulldozer />
               </div>
-            </div>
-            <div className='col-sm'>
-              <div className='container option'>
-                <div className='row'>
-                  <div className='col ofrecemos-icono bg-success'>
-                    <Crane />
-                  </div>
-                  <div className='col ofrecemos-texto bg-warning'>
-                    <p>
-                      <span>{texto.ofrecemos.span.b1}</span> {texto.ofrecemos.span.b2}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='col-sm'>
-              <div className='container option'>
-                <div className='row'>
-                  <div className='col ofrecemos-icono bg-primary'>
-                    <Page />
-                  </div>
-                  <div className='col ofrecemos-texto bg-secondary'>
-                    <p>
-                      <span>{texto.ofrecemos.span.c1}</span> {texto.ofrecemos.span.c2}
-                    </p>
-                  </div>
-                </div>
+              <div className='col ofrecemos-texto'>
+                <p>
+                  <span>{idioma.ofrecemos.span.a1}</span> {idioma.ofrecemos.span.a2}
+                </p>
               </div>
             </div>
           </div>
-          <div className='row ofrecemos-opciones bg-info align-items-center'>
-            <div className='col-sm'>
-              <div className='container option'>
-                <div className='row'>
-                  <div className='col ofrecemos-icono bg-secondary'>
-                    <Machine />
-                  </div>
-                  <div className='col ofrecemos-texto bg-primary'>
-                    <p>
-                      <span>{texto.ofrecemos.span.d1}</span> {texto.ofrecemos.span.d2}
-                    </p>
-                  </div>
-                </div>
+        </div>
+        <div className='col-sm'>
+          <div className='container option'>
+            <div className='row'>
+              <div className='col ofrecemos-icono'>
+                <Crane />
+              </div>
+              <div className='col ofrecemos-texto'>
+                <p>
+                  <span>{idioma.ofrecemos.span.b1}</span> {idioma.ofrecemos.span.b2}
+                </p>
               </div>
             </div>
-            <div className='col-sm'>
-              <div className='container option'>
-                <div className='row'>
-                  <div className='col ofrecemos-icono bg-success'>
-                    <Tow />
-                  </div>
-                  <div className='col ofrecemos-texto bg-warning'>
-                    <p>
-                      <span>{texto.ofrecemos.span.e1}</span> {texto.ofrecemos.span.e2}
-                    </p>
-                  </div>
-                </div>
+          </div>
+        </div>
+        <div className='col-sm'>
+          <div className='container option'>
+            <div className='row'>
+              <div className='col ofrecemos-icono'>
+                <Page />
               </div>
-            </div>
-            <div className='col-sm'>
-              <div className='container'>
-                <div className='row'>
-                  <div className='col ofrecemos-icono bg-secondary'>
-                    <Truck />
-                  </div>
-                  <div className='col ofrecemos-texto bg-primary'>
-                    <p>
-                      <span>{texto.ofrecemos.span.f1}</span> {texto.ofrecemos.span.f2}
-                    </p>
-                  </div>
-                </div>
+              <div className='col ofrecemos-texto'>
+                <p>
+                  <span>{idioma.ofrecemos.span.c1}</span> {idioma.ofrecemos.span.c2}
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+      <div className='row ofrecemos-opciones align-items-center'>
+        <div className='col-sm'>
+          <div className='container option'>
+            <div className='row'>
+              <div className='col ofrecemos-icono'>
+                <Machine />
+              </div>
+              <div className='col ofrecemos-texto'>
+                <p>
+                  <span>{idioma.ofrecemos.span.d1}</span> {idioma.ofrecemos.span.d2}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='col-sm'>
+          <div className='container option'>
+            <div className='row'>
+              <div className='col ofrecemos-icono'>
+                <Tow />
+              </div>
+              <div className='col ofrecemos-texto'>
+                <p>
+                  <span>{idioma.ofrecemos.span.e1}</span> {idioma.ofrecemos.span.e2}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='col-sm'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col ofrecemos-icono'>
+                <Truck />
+              </div>
+              <div className='col ofrecemos-texto'>
+                <p>
+                  <span>{idioma.ofrecemos.span.f1}</span> {idioma.ofrecemos.span.f2}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Ofrecemos;
