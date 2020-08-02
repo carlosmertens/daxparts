94a26797d4fd76c059303ffe4ed8fe7a562866df
Carlos Mertens <mertenscarlos@yahoo.com>
2020-08-02 21:52:51 +0200

    Refactor Spinner from Class to Hook component

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Spinner.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 library.add(faSpinner);
 
-class Spinner extends Component {
-  render() {
-    return (
-      <>
-        <div className='spinner-wrapper'>
-          <FontAwesomeIcon icon='spinner' size='10x' spin />
-        </div>
-        <div className='spinner-title'>
-          <h2>Buscando en Bolivia y el mundo</h2>
-        </div>
-      </>
-    );
-  }
-}
+const Spinner = () => (
+  <>
+    <div className='spinner-wrapper'>
+      <FontAwesomeIcon icon='spinner' size='10x' spin />
+    </div>
+    <div className='spinner-title'>
+      <h2>Buscando en Bolivia y el mundo</h2>
+    </div>
+  </>
+);
 
 export default Spinner;
