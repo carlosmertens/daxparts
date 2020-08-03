1c73688e1ae96bf0e5768d4cb3653fd75aa64248
Carlos Mertens <mertenscarlos@yahoo.com>
2020-08-03 22:30:08 +0200

    Setup Tutorial page

--- src/components/tutorial/Tutorial.js
+++ src/components/tutorial/Tutorial.js
@@ -21,40 +21,126 @@ const Tutorial = () => {
           {/* === Columna 1 === */}
           <div className='col-sm'>
             <div className='container'>
-              <p>
-                Usar Manuales de partes en linea <br />
-                <span>
-                  puedes tener acceso a manuales de manera gratuita.{' '}
-                  <Link to='/hacemos'>
-                    <em className='tutorial-enlace'>Has click para ir al tuturial.</em>
-                  </Link>
-                </span>
-              </p>
+              <div className='row'>
+                <p>
+                  Usar Manuales de partes en linea <br />
+                  <span>
+                    puedes tener acceso a manuales de manera gratuita. <br />
+                    <Link to='/hacemos'>
+                      <em className='tutorial-enlace'>
+                        Has click para ir al tuturial.
+                      </em>
+                    </Link>
+                  </span>
+                </p>
+              </div>
             </div>
             <div className='container'>
               <div className='row'>
-                <iframe
-                  className='tutorial-video'
-                  width='121'
-                  height='65'
-                  title='Video1'
-                  src='https://www.youtube.com/embed/tgbNymZ7vqY'></iframe>
-                <p className='tutorial-video-texto'>Video 1</p>
+                <div className='col'>
+                  <iframe
+                    width='121'
+                    height='65'
+                    title='Video1'
+                    src='https://www.youtube.com/embed/4jnCVogNCtc'></iframe>
+                </div>
+                <div className='col'>
+                  <span className='tutorial-video-texto'>Video 1</span>
+                </div>
+              </div>
+            </div>
+
+            <div className='container'>
+              <div className='row'>
+                <div className='col'>
+                  <iframe
+                    width='121'
+                    height='65'
+                    title='Video1'
+                    src='https://www.youtube.com/embed/4jnCVogNCtc'></iframe>
+                </div>
+                <div className='col'>
+                  <span className='tutorial-video-texto'>Video 2</span>
+                </div>
+              </div>
+            </div>
+
+            <div className='container'>
+              <div className='row'>
+                <div className='col'>
+                  <iframe
+                    width='121'
+                    height='65'
+                    title='Video1'
+                    src='https://www.youtube.com/embed/4jnCVogNCtc'></iframe>
+                </div>
+                <div className='col'>
+                  <span className='tutorial-video-texto'>Video 3</span>
+                </div>
               </div>
             </div>
           </div>
           {/* === Columna 2 === */}
           <div className='col-sm'>
             <div className='container'>
-              <p>
-                como usar nuestra pagina <br />
-                <span>
-                  Te enseñamos como de manera sencilla y breve.{' '}
-                  <Link to='/hacemos'>
-                    <em className='tutorial-enlace'>Has click para ir al tuturial.</em>
-                  </Link>
-                </span>
-              </p>
+              <div className='row'>
+                <p>
+                  Como usar nuestra pagina <br />
+                  <span>
+                    te enseñamos como de manera sencilla y breve. <br />
+                    <Link to='/hacemos'>
+                      <em className='tutorial-enlace'>
+                        Has click para ir al tuturial.
+                      </em>
+                    </Link>
+                  </span>
+                </p>
+              </div>
+            </div>
+
+            <div className='container'>
+              <div className='row'>
+                <div className='col'>
+                  <iframe
+                    width='121'
+                    height='65'
+                    title='Video1'
+                    src='https://www.youtube.com/embed/4jnCVogNCtc'></iframe>
+                </div>
+                <div className='col'>
+                  <span className='tutorial-video-texto'>Video 1</span>
+                </div>
+              </div>
+            </div>
+
+            <div className='container'>
+              <div className='row'>
+                <div className='col'>
+                  <iframe
+                    width='121'
+                    height='65'
+                    title='Video1'
+                    src='https://www.youtube.com/embed/4jnCVogNCtc'></iframe>
+                </div>
+                <div className='col'>
+                  <span className='tutorial-video-texto'>Video 2</span>
+                </div>
+              </div>
+            </div>
+
+            <div className='container'>
+              <div className='row'>
+                <div className='col'>
+                  <iframe
+                    width='121'
+                    height='65'
+                    title='Video1'
+                    src='https://www.youtube.com/embed/4jnCVogNCtc'></iframe>
+                </div>
+                <div className='col'>
+                  <span className='tutorial-video-texto'>Video 3</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
