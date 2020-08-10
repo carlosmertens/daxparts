f6aa871cdb46d77eba8a3ff1d2caa5f1a93cdeb6
Carlos Mertens <mertenscarlos@yahoo.com>
2020-08-10 12:24:10 +0200

    Style responsive Home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -20,48 +20,49 @@ const Home = (props) => {
   }
   return (
     <header>
-      <div className='container-fluid header-contenido'>
+      <div className='container-fluid'>
         <div className='container-fluid d-flex justify-content-between pais-idioma'>
           <Pais country={props.country} />
           <Idioma lang={props.lang} cambiarIdioma={props.cambiarIdioma} />
         </div>
 
-        <div className='container'>
-          <img className='logo-centro' src={logoCentro} alt='Dax Logo' />
-        </div>
+        <div className='header-contenido'>
+          <div className='container'>
+            <img className='logo-centro' src={logoCentro} alt='Dax Logo' />
+          </div>
 
-        <div className='container d-flex justify-content-center'>
-          <form className='form-inline'>
-            <label htmlFor='buscar'>{props.idioma.home.leyendaBuscar}</label>
-            <input
-              type='text'
-              id='buscar'
-              placeholder={props.idioma.home.campoBuscar}
-              value={props.parte}
-              onChange={props.handleChange}
-            />
-            <Link to={`/${buscarParte}`} className='btn btn-buscar'>
-              {props.idioma.home.botonBuscar}
+          <div className='container d-flex justify-content-center'>
+            <form className='form-inline d-flex justify-content-center'>
+              <label>{props.idioma.home.leyendaBuscar}</label>
+              <input
+                type='text'
+                placeholder={props.idioma.home.campoBuscar}
+                value={props.parte}
+                onChange={props.handleChange}
+              />
+              <Link to={`/${buscarParte}`} className='btn btn-buscar'>
+                {props.idioma.home.botonBuscar}
+              </Link>
+            </form>
+          </div>
+
+          <div className='container click-link'>
+            <button
+              type='button'
+              className='button-link'
+              onClick={() => {
+                props.openModal('open', <Noparte idioma={props.idioma} />);
+              }}>
+              <p className=''>{props.idioma.home.enlaceClick}</p>
+            </button>
+          </div>
+
+          <div className='container'>
+            <Link to='/tutorial'>
+              <img className='logo-tutorial' src={IconVideos} alt='Tutorial Logo' />
+              <p className='tutorial-text'>{props.idioma.home.leyendaTutorial}</p>
             </Link>
-          </form>
-        </div>
-
-        <div className='container click-link'>
-          <button
-            type='button'
-            className='button-link'
-            onClick={() => {
-              props.openModal('open', <Noparte idioma={props.idioma} />);
-            }}>
-            <p className='click-link-text'>{props.idioma.home.enlaceClick}</p>
-          </button>
-        </div>
-
-        <div className='container tutorial-link'>
-          <Link to='/tutorial'>
-            <img className='logo-tutorial' src={IconVideos} alt='Tutorial Logo' />
-            <p className='tutorial-text'>{props.idioma.home.leyendaTutorial}</p>
-          </Link>
+          </div>
         </div>
       </div>
     </header>
