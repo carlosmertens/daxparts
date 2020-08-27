3fee6db4c79ef0e174cc10f342a975b03b18ea8e
Carlos Mertens <mertenscarlos@yahoo.com>
2020-08-27 23:25:38 +0200

    refactor from class to hook

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import openModal from '../../actions/openModal';
@@ -6,112 +6,97 @@ import axios from 'axios';
 import navLogo from '../../images/logoNav.png';
 import SignUp from '../signup/SignUp';
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Login = (props) => {
+  const idioma = props.idioma;
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
 
-  handleUser = (e) => this.setState({ user: e.target.value });
-  handlePassword = (e) => this.setState({ password: e.target.value });
+  // Conponent to close the modal
+  const closeModal = () => {
+    props.openModal('closed', '');
+  };
 
-  handleSubmit = async (e) => {
+  // Submit Login
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    this.closeModal();
-    console.log(this.state.user);
-    console.log(this.state.password);
+    closeModal();
+    console.log(user);
+    console.log(password);
     // ***** Call DaxParts API *****
-    const url = 'http://www.wp.daxparts.com/api/sesion/validar';
-    const data = {
-      logususario: this.state.user,
-      clausuario: this.state.password,
-    };
-    const fetchData = async () => {
-      const response = await axios.post(url, data);
-      console.log(response);
-    };
-
-    fetchData();
-
-    // ***** Call Test API *****
-    // const url = `${window.apiHost}/users/login`;
+    // const url = 'http://www.wp.daxparts.com/api/sesion/validar';
     // const data = {
-    //   email: this.state.email,
-    //   password: this.state.password,
+    //   logususario: this.state.user,
+    //   clausuario: this.state.password,
     // };
-
+    // ***** Call Test API *****
+    const url = `${window.apiHost}/users/login`;
+    const data = {
+      email: user,
+      password: password,
+    };
     // Simple POST request
-    // const response = await axios.post(url, data);
-    // console.log(response);
+    const response = await axios.post(url, data);
+    console.log(response);
   };
 
-  // Conectar API
-  //
+  return (
+    <>
+      <div className='modal-logo d-flex justify-content-center'>
+        <img src={navLogo} alt='Dax Logo' />
+      </div>
 
-  closeModal = () => {
-    this.props.openModal('closed', '');
-  };
-
-  render() {
-    const idioma = this.props.idioma;
-    // console.log(this.props);
-    return (
-      <>
-        <div className='modal-logo d-flex justify-content-center'>
-          <img src={navLogo} alt='Dax Logo' />
-        </div>
-
-        <div className='modal-body'>
-          <form onSubmit={this.handleSubmit}>
-            <div className='form-group d-flex justify-content-center'>
-              <input
-                type='text'
-                className='form-control mr-sm-2'
-                placeholder={idioma.ingresar.nombre}
-                onChange={this.handleUser}
-              />
-            </div>
-
-            <div className='form-group d-flex justify-content-center'>
-              <input
-                type='password'
-                className='form-control mr-sm-2'
-                placeholder={idioma.ingresar.password}
-                onChange={this.handlePassword}
-              />
-            </div>
-
-            {/* <div className='enlace-form'>
-              <a className='me-olvide' href='/#'>
-                {idioma.ingresar.olvide}
-              </a>
-            </div> */}
-
-            <div className='boton-form'>
-              <button type='submit' className='btn'>
-                {idioma.ingresar.botonIngresar}
-              </button>
-            </div>
-          </form>
-        </div>
-
-        <div className='modal-footer d-flex justify-content-center'>
-          <div>
-            {idioma.ingresar.cambiarModal}{' '}
-            <span
-              className='pointer'
-              onClick={() => {
-                this.props.openModal('open', <SignUp idioma={this.props.idioma} />);
-              }}
-              style={{ color: '#fca728' }}>
-              {idioma.ingresar.cambiarEnlace}
-            </span>
+      <div className='modal-body'>
+        <form onSubmit={handleSubmit}>
+          <div className='form-group d-flex justify-content-center'>
+            <input
+              type='text'
+              className='form-control mr-sm-2'
+              placeholder={idioma.ingresar.nombre}
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+            />
           </div>
+
+          <div className='form-group d-flex justify-content-center'>
+            <input
+              type='password'
+              className='form-control mr-sm-2'
+              placeholder={idioma.ingresar.password}
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </div>
+
+          {/* <div className='enlace-form'>
+            <a className='me-olvide' href='/#'>
+              {idioma.ingresar.olvide}
+            </a>
+          </div> */}
+
+          <div className='boton-form'>
+            <button type='submit' className='btn'>
+              {idioma.ingresar.botonIngresar}
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className='modal-footer d-flex justify-content-center'>
+        <div>
+          {idioma.ingresar.cambiarModal}{' '}
+          <span
+            className='pointer'
+            onClick={() => {
+              this.props.openModal('open', <SignUp idioma={this.props.idioma} />);
+            }}
+            style={{ color: '#fca728' }}>
+            {idioma.ingresar.cambiarEnlace}
+          </span>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
 
 function mapStateToProps(state) {
   return {
